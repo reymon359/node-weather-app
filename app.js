7d5be46baf470645bd6aa6fcfd8cdc562778124a
ramon morcillo <ramonmorcillocascales@gmail.com>
2018-12-07 15:30:21 +0100

    Implemented npm axios to make http requests

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
+const axios = require('axios');
+
 //I will add the params without command
 const argv = require('yargs').options({
-    adress: {
+    address: {
         alias: 'a',
         desc: 'Adress of the city we want to know the weather',
         demand: true
     }
 }).argv;
 
-console.log(argv.adress);
+//to adapt the parameters to the google api url
+let encodedUrl = encodeURI(argv.address);
+
+axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodedUrl}&key=AIzaSyC8hBRAlbrGCvH26yiXvCRiRMZko5zSRS0`)
+    .then(resp => {
+        // console.log(JSON.stringify(resp.data, undefined, 2));
+        let location = resp.data.results[0];
+        let coors = location.geometry.location;
+
+        console.log('Adress: ', location.formatted_address);
+        console.log('lat', coors.lat);
+        console.log('lng', coors.lng);
+    })
+    .catch(e => console.log('ERROR!!!', e))
